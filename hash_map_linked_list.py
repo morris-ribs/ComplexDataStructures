a1fb7578e0206a884afd4f228b03694dadf74ea3
Mauricio Ribeiro <maumribeiro@gmail.com>
2020-07-03 17:47:01 +0200

    Update hash_map_linked_list.py

--- hash_map_linked_list.py
+++ hash_map_linked_list.py
@@ -18,18 +18,18 @@ class HashMap:
     payload = Node([key, value])
     list_at_array = self.array[array_index]
 
-    for item in list_at_array:
+    for item in list_at_index:
       if item[0] == key:
         item[1] = value
         return
     
-    list_at_array.insert(payload)
+    list_at_index.insert(payload)
 
   def retrieve(self, key):
     array_index = self.compressor(self.hash(key))
     list_at_index = self.array[array_index]
 
-    for item in list_at_array:
+    for item in list_at_index:
       if item[0] == key:
         return item[1]
     return None
